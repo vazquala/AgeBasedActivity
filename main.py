d71ea1b7a10d85f9c846762ed25b45f83c1494fe
Alan Vazquez <vazquala000@warren.k12.in.us>
2024-10-23 09:31:08 -0400

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+age = int(input(""))
+
+if age < 5:
+    print("You should play with toys.")
+elif age >= 5 and age <= 12:
+    print("You can play outdoor game.")
+elif age >= 12 and age <= 17:
+    print("You might enjoy video games.")
+else:
+    print("You can try hiking or traveling.")
